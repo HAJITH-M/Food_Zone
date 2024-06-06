32611b2d9d91990c7f3dd43c11210c1d87ea7b19
HAJITH-M <hajithm2003@gmail.com>
2024-06-06 12:46:53 +0530

    cards created

---
+++ index.js
@@ -0,0 +1,22 @@
+let name = "ram"
+name = "sam"
+console.log(name)
+
+
+var lname = "jii"
+lname = "ravi"
+console.log(lname)
+
+const pi = 3.14
+// pi = "area"
+console.log(pi)
+
+let a = 10
+c = a++
+let b = 50
+d = ++b
+console.log(c)
+console.log(c)
+console.log(a)
+
+console.log
